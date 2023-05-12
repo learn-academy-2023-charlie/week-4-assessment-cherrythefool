5dc899f364c088fc4d07680b29858de8f22a88a8
cherrythefool <sarahandecherry@gmail.com>
2023-05-12 16:34:54 -0600

    2 - challenge 1 complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,28 +16,29 @@
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 
 describe("shuffleOffTheFirst", () => {
   it("removes the first value in an array and rearranges the rest", () => {
-    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-    const expected1 = ["yellow", "blue", "pink", "green"]
-    const expected2 = ["indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
     
     // checks if result includes expected subset in any order, but does not check that first value is not included. Both of our test arrays would pass this with no manipulation, since our outcome will be a subset of the original. However we do need this to remaing true.
-    expect(colors1.toEqual(
-      expect.arrayContaining(expected1)))
-    expect(colors2.toEqual(
-      expect.arrayContaining(expected2)))
+    expect(shuffleOffTheFirst(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+    expect(shuffleOffTheFirst(colors2)).toEqual(expect.arrayContaining(["indigo", "periwinkle", "ochre", "aquamarine", "saffron"]))
 
     // checks if first value in original array does not exist in expected array
-    expect(colors1).toEqual(
-      expect.not.arrayContaining("purple"))
-    expect(colors2).toEqual(
-      expect.not.arrayContaining("chartreuse"))
+    expect(shuffleOffTheFirst(colors1)).toEqual(expect.not.arrayContaining(["purple"]))
+    expect(shuffleOffTheFirst(colors2)).toEqual(expect.not.arrayContaining(["chartreuse"]))
+
+    //currently does not test if actually randomized, will continue and come back to solve
 
   })
 })
+
+//
+
+// good fail!     ReferenceError: shuffleOffTheFirst is not defined
+
 
 // b) Create the function that makes the test pass.
 
@@ -51,13 +52,20 @@ describe("shuffleOffTheFirst", () => {
 // // // math.random gives me .1 through 1 with the .ceil, which is all positive. If I add a *-1 it will give me -1 through -.1
 // // // integers confusing me so *10 and I have 1 - 10 and -10 - -1
 // // // can I shift it halfway? if I have like -5 to +5 could I put that in a sort somehow? for a, b -> randomize result? I think it would just shift the resulting amuont?
-// // // okay I think I'm just going to try without the *10 now and subtract integer wise I guess
+// // // okay I think I'm just going to try without the *10 now that I see it better and subtract integer wise I guess
+// // // still not working, reviewing how these built-ins work
+// // // took off math.ceil and it works
 
 //create and name a function which intakes an array
-// const shuffleOffTheFirst = (array) => {
-//   //slice the array from the 1 index to the end, effectively removing first value. 
-//   return array.slice(1).sort((a, b) => Math.ceil(Math.random()-0.5))
-// }
+const shuffleOffTheFirst = (array) => {
+  // Slice the array from the 1 index to the end, returning all values after the first one as an array.
+  // sort randomly. Sort (a,b) usually determines shifting by a positive or negative result from a-b. This code randomly generates a positive or negative value by shifting the Math.random range (normally 0 - 1) half a step down (-.5 to 0.5) without tying it to the a or b value, so the shifting of a and b is randomly left or right as .sort iterates through the array
+  return array.slice(1).sort((a, b) => Math.random()-0.5)
+}
+
+console.log("test 1:", shuffleOffTheFirst(colors1));
+console.log("test 2:", shuffleOffTheFirst(colors2));
+//I know from this log that I'm actually randomizing, but I also know that my original arrays will currently test as a pass with a slice but no randomization. I'll be coming back to that later.
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
